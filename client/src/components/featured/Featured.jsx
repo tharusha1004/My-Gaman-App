6979346fff3b407944ac174670ef3c3fe30091f0
tharusha1004 <tharusha.astro@gmail.com>
2024-06-09 17:56:25 +0530

    create featured component and add to the home page

---
+++ client/src/components/featured/Featured.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+// import Featured.css 
+import '../featured/Featured.css'
+
+const Featured = () => {
+  return (
+    <div>Featured</div>
+  )
+}
+
+export default Featured
--- client/src/pages/home/Home.jsx
+++ client/src/pages/home/Home.jsx
@@ -5,12 +5,16 @@ import '../home/Home.css';
 //import components
 import Navbar from '../../components/navbar/Navbar';
 import Header from '../../components/header/Header';
+import Featured from '../../components/featured/Featured';
 
 const Home = () => {
     return (
         <div>
             <Navbar />
             <Header />
+            <div className="homeContainer">
+                <Featured/>
+            </div>
         </div>
     )
 }
