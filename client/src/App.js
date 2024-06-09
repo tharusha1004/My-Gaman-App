ec39a6985dc3778fbd99e99031e3ac6357d0608a
tharusha1004 <tharusha.astro@gmail.com>
2024-06-09 13:30:19 +0530

    create main and home pages and add into the app.js

--- client/src/App.js
+++ client/src/App.js
@@ -3,12 +3,16 @@ import './App.css';
 
 //import react-router-dom
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
+// import pages
+import Main from './pages/home/Main';
+import Home from './pages/home/Home';
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route></Route>
+        <Route path='/' element={<Main />} />
+        <Route path='/home' element={<Home />} />
       </Routes>
     </BrowserRouter>
   );
