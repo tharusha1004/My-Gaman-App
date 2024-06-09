1f0d3de989967f0e2377d3407211fc89cec16db2
tharusha1004 <tharusha.astro@gmail.com>
2024-06-09 13:35:28 +0530

    create single hotel page and add into the app.js

--- client/src/App.js
+++ client/src/App.js
@@ -7,6 +7,7 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Main from './pages/home/Main';
 import Home from './pages/home/Home';
 import List from './pages/list/List';
+import Hotel from './pages/hotel/Hotel';
 
 function App() {
   return (
@@ -15,6 +16,7 @@ function App() {
         <Route path='/' element={<Main />} />
         <Route path='/home' element={<Home />} />
         <Route path='/hotels' element={<List />} />
+        <Route path='/hotels/:id' element={<Hotel />} />
       </Routes>
     </BrowserRouter>
   );
