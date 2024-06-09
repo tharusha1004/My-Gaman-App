e4f1432b8bb2b05f6b80bcf9b15ffdb74bfb1864
tharusha1004 <tharusha.astro@gmail.com>
2024-06-09 19:06:16 +0530

    create property list component and styles and add to home page

--- client/src/pages/home/Home.jsx
+++ client/src/pages/home/Home.jsx
@@ -6,6 +6,7 @@ import '../home/Home.css';
 import Navbar from '../../components/navbar/Navbar';
 import Header from '../../components/header/Header';
 import Featured from '../../components/featured/Featured';
+import PropertyList from '../../components/propertylist/PropertyList';
 
 const Home = () => {
     return (
@@ -15,6 +16,7 @@ const Home = () => {
             <div className="homeContainer">
                 <Featured/>
                 <h1 className='homeTitle'>Browse by property type</h1>
+                <PropertyList/>
             </div>
         </div>
     )
