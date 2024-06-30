90988edf018d73434a287f07a5cde986cbc3c272
tharusha1004 <tharusha.astro@gmail.com>
2024-06-30 22:43:39 +0530

    initial step of search card in hotel list is developed

--- client/src/pages/list/List.jsx
+++ client/src/pages/list/List.jsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import '../list/List.css'
 
 //import components
 import Navbar from '../../components/navbar/Navbar'
@@ -7,8 +8,16 @@ import Header from '../../components/header/Header'
 const List = () => {
   return (
     <div>
-      <Navbar/>
-      <Header type="list"/>
+      <Navbar />
+      <Header type="list" />
+      <div className="listContainer">
+        <div className="listWrapper">
+          <div className="listSearch">
+            <h1 className="lsTitle">Search</h1>
+          </div>
+          <div className="listResult"></div>
+        </div>
+      </div>
     </div>
   )
 }
